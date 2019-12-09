3b3b7ce28a23ecb90c0edb945d329ff4484db273
anuragdutt <anurag2709@gmail.com>
2019-12-08 19:31:30 -0500

    Adding savepath for results

--- src/model_mlp.py
+++ src/model_mlp.py
@@ -1,4 +1,3 @@
-## Run only on CPU
 import os
 import sys
 import numpy as np
@@ -6,18 +5,14 @@ import pandas as pd
 import re
 import ast
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-import seaborn as sns
 import gc
 import pickle
 import pandas
 
 import tensorflow as tf
 from keras.models import Sequential, Model, load_model
-from keras.preprocessing.sequence import pad_sequences
 from keras import backend as K
 from keras.optimizers import SGD
-from keras.preprocessing.text import Tokenizer
 from keras.layers import concatenate as lconcat
 from keras.layers import Dense, Dropout 
 from keras.layers import GRU, CuDNNGRU,Input, LSTM, Embedding, Bidirectional
@@ -41,49 +36,9 @@ from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.preprocessing import StandardScaler
+import argparse
 
-global aux_shape
-global vocab_size  
-global embed_dim
-global max_words
 
-def load_embeddings(vec_file):
-    print("Loading Glove Model")
-    f = open(vec_file,'r')
-    model = {}
-    for line in f:
-        splitLine = line.split()
-        word = splitLine[0]
-        embedding = np.array([float(val) for val in splitLine[1:]])
-        model[word] = embedding
-    print("Done. {} words loaded!".format(len(model)))
-    return model
-
-def tokenize_and_pad(docs,max_words=34603):
-    global t
-    t = Tokenizer()
-    t.fit_on_texts(docs)
-    docs = pad_sequences(sequences = t.texts_to_sequences(docs),maxlen = max_words, padding = 'post')
-    global vocab_size
-    vocab_size = len(t.word_index) + 1
-    
-    return docs
-
-def oversample(X,docs,y):
-    # Get number of rows with imbalanced class
-    target = y.sum().idxmax()
-    n = y[target].sum()
-    # identify imbalanced targets
-    imbalanced = y.drop(target,axis=1)
-    #For each target, create a dataframe of randomly sampled rows, append to list
-    append_list =  [y.loc[y[col]==1].sample(n=n-y[col].sum(),replace=True,random_state=20) for col in imbalanced.columns]
-    append_list.append(y)
-    y = pd.concat(append_list,axis=0)
-    # match y indexes on other inputs
-    X = X.loc[y.index]
-    docs = pd.DataFrame(docs_train,index=y_train.index).loc[y.index]
-    assert (y.index.all() == X.index.all() == docs.index.all())
-    return X,docs.values,y
 
 def auc_roc(y_true, y_pred):
     # any tensorflow metric
@@ -101,6 +56,8 @@ def auc_roc(y_true, y_pred):
     with tf.control_dependencies([update_op]):
         value = tf.identity(value)
         return value
+
+
 
 def build_model(output_classes,architecture,embedding_matrix,aux_shape,vocab_size,embed_dim,max_seq_len):
     
@@ -150,26 +107,10 @@ def build_model(output_classes,architecture,embedding_matrix,aux_shape,vocab_siz
     model = Model(inputs=[main_input, auxiliary_input], outputs=[main_output],name=architecture)
         
     #sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model = multi_gpu_model(model)
+    # model = multi_gpu_model(model)
     model.compile('adam', 'categorical_crossentropy',metrics=['accuracy',auc_roc])
     
     return model
-
-def plot_metrics(model_dict,metric,x_label,y_label):
-    plots = 1
-    plt.figure(figsize=[15,10])
-    for model, history in model_dict.items():
-        plt.subplot(2,2,plots)
-        plt.plot(history[metric])
-        #plt.plot(history.history['val_acc'])
-        plt.title('{0} {1}'.format(model,metric))
-        plt.ylabel(y_label)
-        plt.xlabel(x_label)
-        plots += 1
-    #plt.legend(['train', 'test'], loc='upper left')
-    plt.tight_layout()
-    plt.savefig("Graphs/{}.png".format(metric),format="png")
-    plt.show()
     
 def gen():
     print('generator initiated')
@@ -181,117 +122,70 @@ def gen():
 
 
 
-def plot_metrics(model_dict,metric,x_label,y_label):
-    plots = 1
-    plt.figure(figsize=[15,10])
-    for model, history in model_dict.items():
-        plt.subplot(2,2,plots)
-        plt.plot(history[metric])
-        #plt.plot(history.history['val_acc'])
-        plt.title('{0} {1}'.format(model,metric))
-        plt.ylabel(y_label)
-        plt.xlabel(x_label)
-        plots += 1
-    #plt.legend(['train', 'test'], loc='upper left')
-    plt.tight_layout()
-    plt.savefig("Graphs/{}.png".format(metric),format="png")
-    plt.show()
-
 if __name__ == "__main__":
 
-	max_words = 34603
-	embed_dim = 100
-
-	wiki_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-	cik_df = pd.read_html(wiki_url,header=0,index_col=0)[0]
-	cik_df['GICS Sector'] = cik_df['GICS Sector'].astype("category")
-	cik_df['GICS Sub Industry'] = cik_df['GICS Sector'].astype("category")
-
-	cik_df = cik_df.loc[:, ['CIK', 'GICS Sector', 'GICS Sub Industry']]
-	cik_df.columns = ['cik', 'GICS Sector', 'GICS Sub Industry']
-
-	# df = pd.read_pickle("../data/pickles/lemmatized_data.pickle")
-	# print("******************************************************")
-
-	df = pd.read_csv("../data/embedded_data/main_data_3.csv.gz", compression = "gzip")
-	# df = df.loc[:1500]
-	# df.to_csv("../data/embedded_data/sample_data.csv.gz", compression = "gzip", index = False)
-	# df = pd.read_csv("../data/embedded_data/sample_data.csv.gz", compression = "gzip")
-	df = df.dropna()
-	print(df.shape)
-	# exit(0)
-
-	df = pd.merge(df, cik_df, on = "cik", how = "left")
+    parser = argparse.ArgumentParser(description='Train Model')
+    # parser.add_argument('--model', type=str, help="Model to be trained", default="rnn_cnn")
+    parser.add_argument('--batch-size', type=int, help="size of batch for training", default=32)
+    parser.add_argument('--epochs', type=int, help="num epochs for training", default=15)
+    parser.add_argument('--sp-pickles', type=str, help="save folder for all generated pickles", default="../data/pickles")
+    parser.add_argument('--sp-models', type=str, help="save folder for all generated models", default="../data/models")
 
 
-	mlb = MultiLabelBinarizer()
-	df = df.join(pd.DataFrame(mlb.fit_transform(df.pop('items')),columns=mlb.classes_,),sort=False,how="left")
-	
-	cols = ['GICS Sector','vix','rm_week','rm_month', 'rm_qtr', 'rm_year']
-	cols.extend(list(mlb.classes_))
-	X = df[cols]
-	docs = df['processed_text']
-	y = df['signal']	
+    args = parser.parse_args()
+
+    mod_name = "mlp"
+    batch_size = args.batch_size
+    num_epochs = args.epochs
+    sp_pickles = args.sp_pickles
+    sp_model = args.sp_models
 
 
-	# Get Dummies
+    # model_dict = dict()
+    config = os.path.join(sp_pickles, 'config.pkl')
+    fn_train = os.path.join(sp_pickles, 'train_input.pkl')
+    # fn_test = os.path.join(sp_pickles, 'test_input.pkl')
 
-	docs = tokenize_and_pad(docs)
-	X = pd.get_dummies(columns = ['GICS Sector'],prefix="sector",data=X)
-	y = pd.get_dummies(columns=['signal'],data=y)
-
-
-	aux_shape = len(X.columns)
-	X_train, X_test, y_train, y_test, docs_train, docs_test = train_test_split(X, y,docs,
-                                                    stratify=y, 
-                                                    test_size=0.3,
-                                                    random_state = 20)
+    config = pickle.load(open(config, 'rb'))
+    train_dict = pickle.load(open(fn_train, 'rb'))
+    # test_dict = pickle.load(open(fn_test))
 
 
-	cont_features = ['vix','rm_week','rm_month', 'rm_qtr', 'rm_year']
-	aux_features = cont_features + [item for item in mlb.classes_]
-	x_scaler = StandardScaler()
-	X_train[cont_features] = x_scaler.fit_transform(X_train[cont_features])
-	X_test[cont_features] = x_scaler.transform(X_test[cont_features])
+    embedding_matrix = config['embedding_matrix']
+    aux_shape = config['aux_shape']
+    vocab_size = config['vocab_size']
+    embed_dim = config['embed_dim']
+    max_words = config['max_words']    
 
-	X_train, docs_train, y_train = oversample(X_train, docs_train, y_train)
-	embeddings_index = load_embeddings("../data/glove/glove.6B.100d.txt")
-	words_not_found = []
+    docs_train = train_dict['docs_train']
+    X_train = train_dict['X_train']
+    y_train = train_dict['y_train']
 
+    if mod_name in ["mlp"]:
+        mod = build_model(3,mod_name, embedding_matrix = embedding_matrix, aux_shape = aux_shape, vocab_size = vocab_size, embed_dim = embed_dim, max_seq_len = max_words)
+        print(mod_name + ".......................................................")
+        model_fit = mod.fit([docs_train,X_train],y_train,batch_size=batch_size,epochs=num_epochs,verbose=1)
 
-	embedding_matrix = np.zeros((vocab_size, embed_dim))
-	for word, i in t.word_index.items():
-		embedding_vector = embeddings_index.get(word)
-		if embedding_vector is not None:
-		    # words not found in embedding index will be all-zeros.
-		    embedding_matrix[i] = embedding_vector
-		else:
-		    words_not_found.append(word)
+        fn = ''.join([mod_name,'_', str(batch_size), '_', str(num_epochs), '.hdf5'])
+        sp = os.path.join(sp_model, fn)
 
-	print('number of null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
+        mod.save(sp)
+    
+        model_pickle_file = ''.join([mod_name,'_', str(batch_size), '_', str(num_epochs), ".pkl"])
+        model_pickle_path = os.path.join(sp_pickles, model_pickle_file)
 
+        with open(model_pickle_path, 'wb') as file_pi:
+            pickle.dump(model_fit.history, file_pi)
 
+    else:
+        print("Incorrect model name.................please try again")
 
-	# Save data
-	np.save("../data/pickles/docs_train.npy",docs_train)
-	np.save("../data/pickles/docs_test.npy",docs_test)
+    print("Finished training")
 
-	X_train.to_pickle("../data/pickles/X_train.pkl")
-	X_test.to_pickle("../data/pickles/X_test.pkl")
-
-	y_train.to_pickle("../data/pickles/y_train.pkl")
-	y_test.to_pickle("../data/pickles/y_test.pkl") 
-
-	np.save("../data/pickles/embedding_matrix.npy",embedding_matrix)
-
-	model_dict = dict()
-
-
-	mlp = build_model(3,"mlp", embedding_matrix = embedding_matrix, aux_shape = aux_shape, vocab_size = vocab_size, embed_dim = embed_dim, max_seq_len = max_words)
-	print("mlp.......................................................")
-	model_dict["mlp"] = mlp.fit([docs_train,X_train],y_train,batch_size=64, epochs=10,verbose=1) 
-
-	mlp.save("../data/models/mlp.hdf5")
-	# with open('../data/train_history/mlp.pkl', 'wb') as file_pi:
-	    # pickle.dump(model_dict["mlp"], file_pi)
-
+    #### Training for more epochs
+    # history = rnn_cnn.fit(x = [docs_train,X_train],
+    #                   y = y_train,
+    #                   batch_size = 32,
+    #                   epochs = 20,
+    #                   verbose = 1,
+    #                   validation_data = ([docs_test,X_test],y_test))
